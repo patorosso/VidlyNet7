6b4f1c8782dd2bce458b728b8b00b9b9fc17f237
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-04-04 18:12:23 -0300

    add movie: controller, model and view

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mox_mvc_crud.Models;
+
+namespace mox_mvc_crud.Controllers
+{
+	public class MoviesController : Controller
+	{
+		// GET: Movies/Random
+		public IActionResult Random()
+		{
+			var movie = new Movie() { Name = "Inception" };
+
+			return View(movie);
+		}
+		public ActionResult Edit(int id) => Content("id=" + id);
+		//movies
+		public ActionResult Index(int? pageIndex, string sortBy)
+		{
+			if (!pageIndex.HasValue)
+				pageIndex = 1;
+
+			if (String.IsNullOrWhiteSpace(sortBy)) sortBy = "Name";
+
+			return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+		}
+	}
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace mox_mvc_crud.Models
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+	}
+
+
+}
