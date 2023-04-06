6ed07cde2dd693c46a5f370043d10a7f6adc2669
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-04-06 19:53:34 -0300

    finish with customer index

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,13 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VidlyNet7.Models;
 
 namespace VidlyNet7.Controllers
 {
-	public class CustomersController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+    public class CustomersController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Customer> customers = new()
+            {
+                new Customer { Name = "John Smith" },
+                new Customer { Name = "Mary Williams" }
+            };
+
+            return View(customers);
+        }
+
+        public IActionResult Details()
+        {
+
+
+            return View();
+        }
+    }
 }
 
