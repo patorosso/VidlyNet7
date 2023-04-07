58820efeecdeec13ef7caf6a716415ad08dbfe2f
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-04-07 01:28:05 -0300

    add to movie: details action and view

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -23,5 +23,19 @@ namespace VidlyNet7.Controllers
             var movies = _context.Movies.ToList();
             return View(movies);
         }
+
+        [Route("Movies/Details/{paramId}")]
+        public IActionResult Details(int paramId)
+        {
+            var movie = _context.Movies.FirstOrDefault(c => c.Id == paramId);
+
+            if (movie != null)
+                return View(movie);
+            else return NotFound();
+
+
+
+
+        }
     }
 }
