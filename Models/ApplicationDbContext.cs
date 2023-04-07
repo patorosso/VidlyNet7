ec336822107d35f254f5f69b862b09feae2bf104
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-04-06 23:19:15 -0300

    add DbSet to include customers and movies in my db

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -5,5 +5,8 @@ namespace VidlyNet7.Models
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Movie> Movies => Set<Movie>();
     }
 }
