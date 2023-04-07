c371d26c62f2682369bc49d4b9c4f2587631bfa3
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-04-07 01:00:12 -0300

    Populated customers and membershipTypes. Setting up views

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,27 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using VidlyNet7.Models;
 
 namespace VidlyNet7.Controllers
 {
     public class CustomersController : Controller
     {
-        public IEnumerable<Customer> CustomersList()
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController(ApplicationDbContext _context)
         {
-            return new List<Customer>
-            {
-                new Customer { Id= 1, Name = "John Smith" },
-                new Customer { Id= 2, Name = "Mary Williams" }
-            };
+            this._context = _context;
         }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public IActionResult Index()
         {
-            return View(CustomersList());
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            return View(customers);
         }
 
         [Route("Customers/Details/{paramId}")]
         public IActionResult Details(int paramId)
         {
-            var customer = CustomersList().FirstOrDefault(c => c.Id == paramId);
+            var customer = _context.Customers.FirstOrDefault(c => c.Id == paramId);
 
             if (customer != null)
                 return View(customer);
