c7665b2945515fa5d5bd2f49c80f90d392d6bcad
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-04-07 01:48:33 -0300

    add customerViewModel

---
+++ ViewModel/CustomerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using VidlyNet7.Models;
+
+namespace VidlyNet7.ViewModel
+{
+    public class CustomerViewModel
+    {
+        public Customer Customer { get; set; }
+
+        public string MemTypeName { get; set; } = null!;
+    }
+}
