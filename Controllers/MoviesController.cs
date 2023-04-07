4ac78f4e21777d1e3c6fa8dd2c7f0ca1451ddbe0
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-04-07 01:23:13 -0300

    add movies db context

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -5,16 +5,22 @@ namespace VidlyNet7.Controllers
 {
     public class MoviesController : Controller
     {
-        public IActionResult Edit(int id) => Content("id=" + id);
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext _context)
+        {
+            this._context = _context;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
 
         public IActionResult Index()
         {
-            List<Movie> movies = new()
-            {
-                new Movie { Id= 1, Name = "Shrek" },
-                new Movie { Id= 2, Name = "Wall-e" }
-            };
-
+            var movies = _context.Movies.ToList();
             return View(movies);
         }
     }
