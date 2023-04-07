5d35bfad8fec0b0c1dd2fd9a54790b998c98c198
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-04-07 01:15:53 -0300

    add atributes to Movie class

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,6 +7,13 @@ namespace VidlyNet7.Models
         public int Id { get; set; }
         [StringLength(maximumLength: 50)]
         public string Name { get; set; } = null!;
+        [StringLength(maximumLength: 50)]
+        public string Genre { get; set; } = null!;
+
+        public DateTime ReleaseDate { get; set; }
+        public DateTime DateAdded { get; set; }
+
+        public short Stock { get; set; }
     }
 
 
