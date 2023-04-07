76212a1658ef23c57a590b43f3c616983aabff9a
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-04-06 23:39:21 -0300

    working db migrations. change string connection to program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllersWithViews();
 var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
 
 builder.Services.AddDbContext<ApplicationDbContext>
-    (options => options.UseMySql("DefaultConnection", serverVersion));
+    (options => options.UseMySql("Server=localhost;Port=3306;Database=vidly;Uid=root;Pwd=cordillera;", serverVersion));
 
 var app = builder.Build();
 
