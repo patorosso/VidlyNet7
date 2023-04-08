fc34ca7210c5a3ed793b603a8edb33b6345461d5
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-04-08 14:34:18 -0300

    all methods are async now

--- Controllers/Api/MoviesController.cs
+++ Controllers/Api/MoviesController.cs
@@ -19,9 +19,9 @@ namespace VidlyNet7.Controllers.Api
         // GET /api/movies/
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<IEnumerable<Movie>> GetMovies()
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
         {
-            return Ok(_context.Movies.ToList());
+            return Ok(await _context.Movies.ToListAsync());
         }
 
 
@@ -30,12 +30,12 @@ namespace VidlyNet7.Controllers.Api
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<Movie> GetMovie(int id)
+        public async Task<ActionResult<Movie>> GetMovie(int id)
         {
             if (id == 0)
                 return BadRequest();
 
-            var movie = _context.Movies.SingleOrDefault(x => x.Id == id);
+            var movie = await _context.Movies.SingleOrDefaultAsync(x => x.Id == id);
 
             if (movie == null)
                 return NotFound();
@@ -49,7 +49,7 @@ namespace VidlyNet7.Controllers.Api
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public ActionResult<Movie> PostMovie([FromBody] Movie movie)
+        public async Task<ActionResult<Movie>> PostMovie([FromBody] Movie movie)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
@@ -60,7 +60,7 @@ namespace VidlyNet7.Controllers.Api
             if (movie.Id == 0)
                 return StatusCode(StatusCodes.Status500InternalServerError);
 
-            var dbId = _context.Movies.SingleOrDefault(c => c.Id == movie.Id);
+            var dbId = await _context.Movies.SingleOrDefaultAsync(c => c.Id == movie.Id);
 
             if (dbId != null && movie.Id == dbId.Id)
             {
@@ -68,8 +68,8 @@ namespace VidlyNet7.Controllers.Api
                 return BadRequest(ModelState);
             }
 
-            _context.Movies.Add(movie);
-            _context.SaveChanges();
+            await _context.Movies.AddAsync(movie);
+            await _context.SaveChangesAsync();
 
             return CreatedAtAction(nameof(GetMovie), new { id = movie.Id }, movie);
         }
@@ -80,18 +80,18 @@ namespace VidlyNet7.Controllers.Api
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult UpdateMovie(int id, [FromBody] Movie movie)
+        public async Task<IActionResult> UpdateMovie(int id, [FromBody] Movie movie)
         {
             if (id != movie.Id || movie == null)
                 return BadRequest();
 
-            var movieInDb = _context.Movies.AsNoTracking().SingleOrDefault(x => x.Id == id);
+            var movieInDb = await _context.Movies.AsNoTracking().SingleOrDefaultAsync(x => x.Id == id);
 
             if (movieInDb == null)
                 return NotFound();
 
             _context.Movies.Update(movie);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
             return NoContent();
         }
 
@@ -101,18 +101,18 @@ namespace VidlyNet7.Controllers.Api
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult DeleteMovie(int id)
+        public async Task<IActionResult> DeleteMovie(int id)
         {
             if (id == 0)
                 return BadRequest();
 
-            var movie = _context.Movies.SingleOrDefault(x => x.Id == id);
+            var movie = await _context.Movies.SingleOrDefaultAsync(x => x.Id == id);
 
             if (movie == null)
                 return NotFound();
 
-            _context.Movies.ExecuteDeleteAsync();
-
+            _context.Movies.Remove(movie);
+            await _context.SaveChangesAsync();
             return NoContent();
         }
 
