87427be5681313bc30cd98e2d209a0ea66be62cf
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-04-07 23:54:18 -0300

    add two get methods and a post

--- Controllers/Api/MoviesController.cs
+++ Controllers/Api/MoviesController.cs
@@ -15,21 +15,56 @@ namespace VidlyNet7.Controllers.Api
         }
 
 
-        public IEnumerable<Movie> GetMovies()
+        // GET /api/movies/
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<Movie>> GetMovies()
         {
-            return _context.Movies.ToList();
+            return Ok(_context.Movies.ToList());
         }
 
-        public Movie GetMovie(int id)
+
+        // GET /api/movies/id
+        [HttpGet("id:int")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Movie> GetMovie(int id)
         {
+            if (id == 0)
+                return BadRequest();
+
             var movie = _context.Movies.SingleOrDefault(x => x.Id == id);
 
             if (movie == null)
-            {
+                return NotFound();
 
-                throw new HttpRequestException();
-            }
-            return movie;
+            return Ok(movie);
         }
+
+
+        // POST /api/movies
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<Movie> PostMovie([FromBody] Movie movie)
+        {
+            if (movie == null)
+                return BadRequest(movie);
+
+            if (movie.Id > 0)
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+
+            _context.Movies.Add(movie);
+
+            return CreatedAtAction(nameof(GetMovie), new { id = movie.Id }, movie);
+
+
+        }
+
+
+
     }
 }
